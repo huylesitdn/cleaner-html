ae59f0e6a51226b4969d6794aa0b79c50ebbda46
Huy Le <mrsjiodev@gmail.com>
2023-10-30 15:20:50 +0700

    update

--- js/swiper-multi-item.js
+++ js/swiper-multi-item.js
@@ -1,8 +1,38 @@
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 4,
   spaceBetween: 20,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
-    clickable: true
-  }
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    300: {
+      slidesPerView: 1,
+      spaceBetween: 16,
+    },
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 16,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 16,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1440: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+  },
 });
